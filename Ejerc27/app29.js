b80fc3be4b1f63e8f3f20d17c9bb2df6c06c2d4b
alberola <alejandroalberola140495@gmail.com>
2023-03-23 12:25:15 +0000

    Exec 29 Loading txt and html files with jquery

---
+++ Ejerc27/app29.js
@@ -0,0 +1,39 @@
+$('#btn1').click( () => {
+    $('.output').load('file29.html', (rep, status, xhr) => {
+        if ( status == 'success') {
+            console.log(rep,status);
+            console.log('SUCCESS!!!!');
+        } else {
+            console.log(rep,status);
+            console.log('Help!!!!');
+        }
+
+        console.log(xhr);
+    });
+})
+$('#btn2').click( () => {
+    $('.output').load('file29a.txt', (rep, status, xhr) => {
+        if ( status == 'success') {
+            console.log(rep,status);
+            console.log('SUCCESS!!!!');
+        } else {
+            console.log(rep,status);
+            console.log('Help!!!!');
+        }
+
+        console.log(xhr);
+    });
+})
+$('#btn3').click( () => {
+    $('.output').load('file29b.txt', (rep, status, xhr) => {
+        if ( status == 'success') {
+            console.log(rep,status);
+            console.log('SUCCESS!!!!');
+        } else {
+            console.log(rep,status);
+            console.log('Help!!!!');
+        }
+
+        console.log(xhr);
+    });
+})
